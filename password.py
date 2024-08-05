3f8ffb3603904ddcbd0f0a86abde4acc34ec43e8
Hassan Ahmed Qureshi <codewithminds@gmail.com>
2024-08-05 15:53:02 +0500

    Small Project

---
+++ password.py
@@ -0,0 +1,43 @@
+from cryptography.fernet import Fernet
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+
+master_pwd = input("Enter your master password: ")
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+def view():
+    with open('passswords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print(f"User: {user}, Password: {fer.decrypt (passw.encode())}").decode
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open('passswords.txt', 'a') as f:
+        f.write(f"{name} | {fer.encrypt(pwd.encode()).decode()} \n")
+
+while True:
+    mode = input("Would you like to add a new password or view exiting one (view or add) or q to quiet. ")
+    if mode == "q":
+        break
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
